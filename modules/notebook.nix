e0d8e62d6f0a26e7c1963b1b4749425e7f6220fe
Daniel Vollrath <daniel@danielvollrath.de>
2025-12-09 20:23:49 +0100

    Added notebook powermanagement

---
+++ modules/notebook.nix
@@ -0,0 +1,36 @@
+{ inputs, config, lib, pkgs, pkgs-unstable, ... }:
+
+{
+  services.upower.enable = true;
+  environment.systemPackages = with pkgs; [
+    upower
+    powertop  # handy CLI monitor/tuner
+  ];
+
+  programs.auto-cpufreq.enable = true;
+
+  boot = {
+    # Better PCIe link power management on many laptops
+    kernelParams = [ "pcie_aspm.policy=powersave" ];
+
+    extraModprobeConfig = ''
+      # Audio power saving
+      options snd_hda_intel power_save=1
+
+      # Wi‑Fi power saving (Intel example)
+      options iwlwifi power_save=1 d0i3_disable=0 uapsd_disable=0
+    '';
+
+    kernel.sysctl = {
+      "kernel.nmi_watchdog" = 0;
+      "vm.dirty_writeback_centisecs" = 6000;
+      "vm.laptop_mode" = 5;
+    };
+  };
+
+  services.udev.extraRules = ''
+    # Disable Wake‑on‑LAN for wired, enable Wi‑Fi power save
+    ACTION=="add", SUBSYSTEM=="net", KERNEL=="eth*",  RUN+="${pkgs.ethtool}/bin/ethtool -s %k wol d"
+    ACTION=="add", SUBSYSTEM=="net", KERNEL=="wlan*", RUN+="${pkgs.iw}/bin/iw dev %k set power_save on"
+  '';
+}
