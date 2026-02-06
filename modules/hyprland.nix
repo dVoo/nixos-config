196408ac3316657d85d6842bced2ea2008bea458
Daniel Vollrath <daniel@danielvollrath.de>
2026-02-06 21:58:08 +0100

    Added dpms autowake

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -114,6 +114,8 @@ let
     misc = {
       force_default_wallpaper = 0;
       disable_hyprland_logo = true;
+      mouse_move_enables_dpms = true;
+      key_press_enables_dpms = true;
     };
 
     # Input settings
